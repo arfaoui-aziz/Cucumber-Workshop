5104fc7d35b104751e3686d65031ec1c15ee5b5a
arfaoui-aziz <mohamedaziz.arfaoui@esprit.tn>
2022-03-21 14:48:48 +0100

    add Person creation Background

--- CucumberBDD/src/test/java/StepDefinitions.java
+++ CucumberBDD/src/test/java/StepDefinitions.java
@@ -1,17 +1,13 @@
-import io.cucumber.java.PendingException;
-import io.cucumber.java.en.And;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
-import org.junit.Before;
 
 import java.util.HashMap;
 
 public class StepDefinitions {
 
-    private Person sean;
-    private Person lucy;
 
     private HashMap<String,Person> people;
 
@@ -19,32 +15,31 @@ public class StepDefinitions {
 
     @Before
     public void initPeople() {
-        people = new HashMap<String,Person>();
+        people = new HashMap<>();
     }
 
+
     @Given("a person named {person}")
-    public void aPersonNamed(Person person) throws Throwable {
+    public void a_person_named(Person person) {
+        System.out.println(person);
+        System.out.println(person.name);
+
         people.put(person.name,person);
         System.out.println(people);
     }
 
-    @Given("{person} is located/standing {int} meter(s) from Sean")
-    public void lucy_is_located_meters_from_sean(Person person,Integer distance) {
 
-        people.get("Lucy").moveTo(distance);
-
-    }
     @When("Sean Shouts {string}")
     public void sean_shouts(String message) {
       this.message = message;
         people.get("Sean").shout(message);
         people.get("Lucy").setShoutedMessage(message);
     }
+
     @Then("Lucy hears Sean's message")
     public void lucy_hears_sean_s_message() {
         Assert.assertEquals(message,people.get("Lucy").getMessagesHeard());
     }
-
 
 
 
--- CucumberBDD/target/test-classes/hear_shout.feature
+++ CucumberBDD/target/test-classes/hear_shout.feature
@@ -3,16 +3,21 @@ Feature: Shout
   ToDo:
    - only shout to people within a certain range
 
+  Background:
+    Given  a person named Lucy
+    And a person named Sean
+
  Rule: Shouts can be heard by other users
    Scenario: Listener hears a message
-     Given a person named Lucy
-     And a person named Sean
+    When Sean Shouts "free bagels at Sean's"
+    Then Lucy hears Sean's message
+
 #  Scenario: Listener is within range
 #    Given Lucy is located 15 meter from Sean
 #    When Sean Shouts "free bagels at Sean's"
 #    Then Lucy hears Sean's message
 
-  Scenario: Listener is within range
-    Given Lucy is standing 15 meter from Sean
-    When Sean Shouts "free coffe"
-    Then Lucy hears Sean's message
+  #Scenario: Listener is within range
+   # Given Lucy is standing 15 meter from Sean
+    #When Sean Shouts "free coffe"
+    #Then Lucy hears Sean's message
